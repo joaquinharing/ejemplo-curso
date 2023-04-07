a32df0017b578e0c8bc44b571363fd09348b56aa
unknown <joaquin.haring4@gmail.com>
2023-04-07 18:39:16 -0300

    primera subida...

---
+++ Desafío 1/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.ConstrainedExecution;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Desafío_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Crear la clase Telefono(recordemos que una clase va en un archivo nuevo; click derecho en el proyecto, agregar, class).
+            Telefono t1 = new Telefono("A33", "Samsung");
+            //Crear algunos objetos en el main de Program y probar escribirle datos y mostrar en pantalla el estado del Telefono.
+            t1.NumeroTelefonico = "222 444 5555";
+            t1.CodigoOperador = 2;
+            string modelo = t1.Modelo;
+            string marca = t1.Marca;
+
+            Console.WriteLine("mi número es " + t1.NumeroTelefonico);
+            //Probar métodos en el main mostrando en pantalla el comportamiento de los objetos.
+
+            Console.WriteLine(t1.llamar("maxiprograma"));
+            Console.WriteLine("mi teléfono es el modelo " + modelo + " de la marca " + marca);
+        }
+    }
+}
---
+++ Desafío 1/Telefono.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafío_1
+{
+    internal class Telefono
+    {
+        //Agregale los siguientes atributos:
+        //Modelo string.
+        //Marca string.
+        //NumeroTelefonico string.
+        //CodigoOperador int (1, 2 o 3).
+        private string modelo;
+        private string marca;
+        private string numeroTelefonico;
+        private int codigoOperador;
+
+        //Agregale las propiedades correspondientes.Podés hacer el formato largo o el corto.
+        //Modelo: solo lectura. Es decir, solo get.
+        //Marca: solo lectura.
+        //NumeroTelefonico: lectura y escritura.
+        //CodigoOperador: lectura y escritura.Validar escritura que solo admita 1, 2 o 3, caso contrario escribir un cero.
+
+        //PROPIEDADES
+        public string Modelo
+        {
+            get { return modelo; }
+        }
+
+        public string Marca
+        {
+            get { return marca; }
+        }
+
+        public string NumeroTelefonico { get; set; }
+
+        public int CodigoOperador
+        {
+            get { return codigoOperador; }
+            set {
+                if (value > 0 && value <= 3)
+                    codigoOperador = value;
+                else
+                    codigoOperador = 0;
+                }
+            
+        }
+
+        //Agregar Constructor que reciba Modelo y Marca.
+
+        //CONSTRUCTOR
+        public Telefono(string modelo, string marca)
+        {
+            this.modelo = modelo;
+            this.marca = marca;
+        }
+
+        
+
+
+        //Agregar método Llamar() sin parámetros que devuelva un string con la leyenda "Realizando llamada...".
+
+        //MÉTODOS
+        public string llamar()
+        {
+            return "Realizando llamada a ";
+        }
+        //Sobrecargar el método Llamar(string contacto) para que reciba un contacto y devuelva un string con la leyenda "Llamando a " + contacto
+        public string llamar(string contacto)
+        {
+            return "llamando a " + contacto;
+        }
+
+
+    }
+}
